876ba1c27584edae6832381688e743cd6771a0f6
Simon Jefferies <simon@olivegame.studio>
2024-07-22 09:02:22 +0100

    Utility project imported.

---
+++ Utility.Result/Result.cs
@@ -0,0 +1,50 @@
+﻿namespace Utility;
+
+/// <summary>
+/// A result from an task which can be a success or failure.
+/// Where a failure, a reason can be supplied.
+/// </summary>
+public class Result
+{
+    public bool Success { get; }
+
+    public bool IsFailure => !Success;
+
+    public string Error { get; private set; }
+
+    protected Result(bool success, string error)
+    {
+        Success = success;
+        Error = error;
+    }
+
+    public static Result Fail(string message)
+    {
+        return new Result(false, message);
+    }
+
+    public static Result<T> Fail<T>(string message)
+    {
+        return new Result<T>(default(T), false, message);
+    }
+
+    public static Result Ok()
+    {
+        return new Result(true, string.Empty);
+    }
+
+    public static Result<T> Ok<T>(T value)
+    {
+        return new Result<T>(value, true, string.Empty);
+    }
+}
+
+public class Result<T> : Result
+{
+    public T Value { get; set; }
+
+    protected internal Result(T value, bool success, string error) : base(success, error)
+    {
+        Value = value;
+    }
+}
